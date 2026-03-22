95ff5028e89c86ae96e3974a4ba9906943bfa1b8
Bhav Shah <bhav@Bhavs-MacBook-Air-2.local>
2026-03-22 18:37:32 +0530

    Completed Day 1 backend setup with MongoDB

--- server/server.js
+++ server/server.js
@@ -15,6 +15,6 @@ app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(8000, () => {
+  console.log("Server running on port 8000");
 });
