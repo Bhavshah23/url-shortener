4a70d565c89fdf907cd8fb4b0d9b65f9c12ade2e
Bhav Shah <bhav@Bhavs-MacBook-Air-2.local>
2026-03-22 18:28:40 +0530

    Setup backend server and project structure

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("YOUR_MONGO_URI")
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
