cef0d8edd8ad788bdc0a98a2f04c0b97653ce439
Bhav Shah <bhav@Bhavs-MacBook-Air-2.local>
2026-03-22 18:57:26 +0530

    Implemented URL redirection and click tracking

--- server/server.js
+++ server/server.js
@@ -18,6 +18,21 @@ app.get("/", (req, res) => {
   res.send("API Running");
 });
 
+const Url = require("./models/Url");
+
+app.get("/:shortCode", async (req, res) => {
+  const url = await Url.findOne({ shortCode: req.params.shortCode });
+
+  if (url) {
+    url.clicks++;
+    await url.save();
+
+    return res.redirect(url.originalUrl);
+  } else {
+    return res.status(404).send("URL not found");
+  }
+});
+
 app.listen(8000, () => {
   console.log("Server running on port 8000");
 });
